15a26b9887ef38f336aaf64116cc21cfc8c64100
Nikita-Hritsay <hritsaynikita@gmail.com>
2024-01-07 22:33:18 +0200

    separate example methods on different files

--- examples/image_classification_tensorflow.py
+++ examples/image_classification_tensorflow.py
@@ -1,27 +1,6 @@
-import cv2
 import numpy as numpy
 import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, layers, models
-
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-
-## function that cuts image by face countor
-def cut_face():
-    frame = cv2.imread("./eyeTrainDataset/left.png", cv2.CAP_DSHOW)
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        frame = frame[y:y+h, x:x+w]
-
-    cv2.imshow('Eye Tracking', frame)
-
-    cv2.waitKey(0)
 
 ## function that uses tensorflow to classify the image
 def image_classification_using_tensorflow():
@@ -74,7 +53,4 @@ def image_classification_using_tensorflow():
     ## to get the trained model we can use this
     ## model = models.load_model("image_classification.model")
 
-
-
 image_classification_using_tensorflow()
-
